b02a762ccec5249a2ae8e50cbaff2d144b4840e2
Logon Aluno <logonaluno@local.com>
2019-11-04 23:36:34 -0200

    Mandar mensagem implementação

---
+++ LabelLoader/Services/WaitingImageService.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GeekBurger.LabelLoader.Services
+{
+    public class WaitingImageService : IHostedService, IDisposable
+    {
+        public IConfiguration Configuration { get; }
+        const string TopicName = "labelloader";
+        private int executionCount = 0;
+        private readonly ILogger<WaitingImageService> _logger;
+        private Timer _timer;
+        private ITopicClient topicClient;
+        private ManagementClient managementClient;
+
+        public WaitingImageService(ILogger<WaitingImageService> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            Configuration = configuration;
+        }
+
+
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Serviço Iniciado.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,
+                TimeSpan.FromSeconds(60));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            DirectoryInfo _dirNotRead = new DirectoryInfo(Configuration.GetSection("Files")["NotRead"]);
+            FileInfo[] _filesNotRead = _dirNotRead.GetFiles();
+
+            if (_filesNotRead.Length != 0)
+            {
+                VisionServices _vision = new VisionServices(Configuration);
+                foreach (var file in _filesNotRead)
+                {
+                    _vision.ObterIngredientes(file.FullName);
+                    //MainAsync().GetAwaiter().GetResult();  MANDA A MENSAGEM
+                }
+            }
+            
+            _logger.LogInformation("Count: {Count}", executionCount++);
+        }
+
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Serviço Finalizado.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MainAsync()
+        {
+            const int numberOfMessages = 10;
+            try
+            {
+                await managementClient.GetTopicAsync(TopicName);
+            }
+            catch (MessagingEntityNotFoundException)
+            {
+                await managementClient.CreateTopicAsync(new TopicDescription(TopicName) { EnablePartitioning = true });
+            }
+            topicClient = new TopicClient(Configuration.GetSection("AzureServiceBusConfig")["connectionString"], TopicName);
+
+            Console.WriteLine("=======================================================");
+            Console.WriteLine("Press ENTER key to exit after sending all the messages.");
+            Console.WriteLine("=======================================================");
+
+            // Send messages.
+            await SendMessagesAsync(numberOfMessages);
+
+            Console.ReadKey();
+
+            await topicClient.CloseAsync();
+        }
+        private async Task SendMessagesAsync(int numberOfMessagesToSend)
+        {
+            try
+            {
+                for (var i = 0; i < numberOfMessagesToSend; i++)
+                {
+                    // Create a new message to send to the topic.
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console.
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the topic.
+                    await topicClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
--- LabelLoader/Startup.cs
+++ LabelLoader/Startup.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using GeekBurger.LabelLoader.Migrations;
+using GeekBurger.LabelLoader.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,17 +22,15 @@ namespace LabelLoader
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddDbContext<LabelContext> (option =>
             {
                 option.UseSqlServer(Configuration.GetConnectionString("LabelContextConnectionString"));
@@ -37,6 +39,11 @@ namespace LabelLoader
             Log.Logger = new LoggerConfiguration()
                 .ReadFrom.Configuration(Configuration)
                 .CreateLogger();
+            services.AddHostedService<WaitingImageService>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Label Loader", Version = "v1" });
+            });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
@@ -44,6 +51,12 @@ namespace LabelLoader
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Label Loader v1");
+                c.RoutePrefix = string.Empty;
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
